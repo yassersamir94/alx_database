e4f008bb357adf7dfbf3e0038d7df88ca6cca5ff
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-21 22:14:39 -0500

    0

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,7 +1,4 @@
-guillaume@ubuntu:~/$ cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
-Enter password: 
-Database
-information_schema
-mysql
-performance_schema
-guillaume@ubuntu:~/$ 
+-- 0. List databases
+-- This script lists all databases of your MySQL server.
+
+SHOW DATABASES;
