3dfcd2d52ae11efc7c287968256ea9ebe6ffba35
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-22 08:49:11 -0500

    33

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,6 +1,4 @@
 -- 3. List tables
--- This script lists all the tables of a specified database in your MySQL server.
-
-USE mysql;
+-- This script lists all the tables of a database in your MySQL server.
 
 SHOW TABLES;
