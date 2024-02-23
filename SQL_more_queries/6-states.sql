6ad239fd265411e33120570f3e385299bb786976
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-23 00:03:45 -0500

    6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -4,6 +4,9 @@
 -- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use the database
+USE hbtn_0d_usa;
+
 -- Create the table if it does not exist
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
