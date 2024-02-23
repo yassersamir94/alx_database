eee1169701f1c7526c32115454f5e3885109fc90
Yasser Samir <yassersamir.94@hotmail.com>
2024-02-22 23:58:05 -0500

    4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,6 @@
 -- 4. ID can't be null
 -- This script creates the table id_not_null on the MySQL server.
 
--- Select the database
-USE hbtn_0d_2;
-
 -- Create the table if it does not exist
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT NOT NULL DEFAULT 1,
